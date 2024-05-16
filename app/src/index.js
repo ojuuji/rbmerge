acd0f4b4f2e8b25e99a0e63c387d0005b176264d
Mikalai Ananenka <ojuuji@gmail.com>
2024-05-16 03:31:16 +0300

    app: add service worker

--- app/src/index.js
+++ app/src/index.js
@@ -3,6 +3,7 @@ import ReactDOM from 'react-dom/client';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './index.css';
 import App from './App';
+import * as serviceWorkerRegistration from './serviceWorkerRegistration';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 
@@ -11,3 +12,10 @@ root.render(
     <App />
   </React.StrictMode>
 );
+
+if (process.env.NODE_ENV === 'production') {
+  serviceWorkerRegistration.register();
+}
+else {
+  serviceWorkerRegistration.unregister();
+}

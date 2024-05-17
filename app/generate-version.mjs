7cc42bcefd1ef4c2fd4985183dc2ae1eae83e5ae
Mikalai Ananenka <ojuuji@gmail.com>
2024-05-17 20:38:56 +0300

    app: precache all files in public directory

--- app/generate-version.mjs
+++ app/generate-version.mjs
@@ -1,12 +1,19 @@
 import { execSync } from 'node:child_process';
-import { writeFileSync } from 'node:fs';
-import { dirname } from 'node:path';
+import fs from 'node:fs';
+import path from 'node:path';
 import { fileURLToPath } from 'node:url';
 
-const dir = dirname(fileURLToPath(import.meta.url));
+const dir = path.dirname(fileURLToPath(import.meta.url));
 const ts = execSync(`git log -1 --format="%at" "${dir}" "${dir}/../tables/*.csv"`);
 const date = new Date(ts.toString() * 1000);
 const version = date.toISOString().replace('T', '.').replace(/:/g, '-').slice(0, -5);
 console.log(`Version generated: ${version}`);
 
-writeFileSync(`${dir}/.env.local`, `REACT_APP_VERSION=${version}`);
+const publicFiles = fs.readdirSync(`${dir}/public`).filter(f => f != 'index.html').join(',');
+const publicRev = execSync(`git log -1 --format="%H" "${dir}/public"`);
+
+fs.writeFileSync(`${dir}/.env.local`, `
+REACT_APP_VERSION=${version}
+REACT_APP_PUBLIC_FILES=${publicFiles}
+REACT_APP_PUBLIC_REV=${publicRev}
+`);
